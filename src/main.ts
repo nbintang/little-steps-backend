8eafd5f2d1b3e63876c64a57bda1dda1e64ec702
nbintang <nbintangh@gmail.com>
2025-09-21 14:23:24 +0700

    feat(bootstrap): added config service inside bootstrap, added new validation custom pipe

--- src/main.ts
+++ src/main.ts
@@ -3,28 +3,38 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
-import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from './config/config.service';
+import { CustomValidationPipe } from './common/pipes/custom-validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api');
+  const config = app.get(ConfigService);
+
   app.enableCors({
-    origin: [process.env.PROD_BACKEND_URL],
+    origin: [config.backendUrl],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
+
   app.useBodyParser('json', { limit: '10mb' });
   app.useBodyParser('urlencoded', { extended: true });
+
   app.useGlobalPipes(
-    new ValidationPipe({
+    new CustomValidationPipe({
       transform: true,
       whitelist: true,
       forbidNonWhitelisted: true,
+      skipMissingProperties: false,
+      skipNullProperties: false,
+      skipUndefinedProperties: false,
     }),
   );
+
   app.use(cookieParser());
   app.use(compression());
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(config.port ?? 3000);
 }
+
 bootstrap();
